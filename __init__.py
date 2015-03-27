888479a24444639c4ce6f506ebce3047b3e9f4ef
Alexander Scheurer <mail@aspepex.net>
2015-03-27 14:06:48 +0100

    Most basic plugin settings

--- __init__.py
+++ __init__.py
@@ -19,203 +19,23 @@
 # <pep8-80 compliant>
 
 bl_info = {
-    "name": "1 Awesome Fusee Exporter (.fus)",
+    "name": "1 Awesome Fusee Uniplug GUI dodad",
     "author": "The super awesome Fusee team",
-    "version": (2, 0, 0),
-    "blender": (2, 58, 0),
-    "location": "File > Import-Export",
-    "description": "Import-Export OBJ, Import OBJ mesh, UV's, "
-                   "materials and textures",
-    "warning": "",
-    "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/"
-                "Scripts/Import-Export/Wavefront_OBJ",
-    "support": 'OFFICIAL',
-    "category": "Import-Export"}
-
-if "bpy" in locals():
-    import imp
-    if "export_obj" in locals():
-        imp.reload(export_obj)
-
-
-import bpy
-from bpy.props import (BoolProperty,
-                       FloatProperty,
-                       StringProperty,
-                       EnumProperty,
-                       )
-from bpy_extras.io_utils import (ExportHelper,
-                                 path_reference_mode,
-                                 axis_conversion,
-                                 )
-
-class ExportOBJ(bpy.types.Operator, ExportHelper):
-    """Save a Wavefront OBJ File"""
-
-    bl_idname = "export_scene.fus"
-    bl_label = 'Export FUS'
-    bl_options = {'PRESET'}
-
-    filename_ext = ".fus"
-    filter_glob = StringProperty(
-            default="*.fus",
-            options={'HIDDEN'},
-            )
-
-    # context group
-    use_selection = BoolProperty(
-            name="Selection Only",
-            description="Export selected objects only",
-            default=False,
-            )
-    use_animation = BoolProperty(
-            name="Animation",
-            description="Write out an FUS for each frame",
-            default=False,
-            )
-
-    # object group
-    use_mesh_modifiers = BoolProperty(
-            name="Apply Modifiers",
-            description="Apply modifiers (preview resolution)",
-            default=True,
-            )
-
-    # extra data group
-    use_edges = BoolProperty(
-            name="Include Edges",
-            description="",
-            default=True,
-            )
-    use_smooth_groups = BoolProperty(
-            name="Smooth Groups",
-            description="Write sharp edges as smooth groups",
-            default=False,
-            )
-    use_smooth_groups_bitflags = BoolProperty(
-            name="Bitflag Smooth Groups",
-            description="Same as 'Smooth Groups', but generate smooth groups IDs as bitflags "
-                        "(produces at most 32 different smooth groups, usually much less)",
-            default=False,
-            )
-    use_normals = BoolProperty(
-            name="Write Normals",
-            description="Export one normal per vertex and per face, to represent flat faces and sharp edges",
-            default=False,
-            )
-    use_uvs = BoolProperty(
-            name="Include UVs",
-            description="Write out the active UV coordinates",
-            default=True,
-            )
-    use_materials = BoolProperty(
-            name="Write Materials",
-            description="Write out the MTL file",
-            default=True,
-            )
-    use_triangles = BoolProperty(
-            name="Triangulate Faces",
-            description="Convert all faces to triangles",
-            default=False,
-            )
-    use_nurbs = BoolProperty(
-            name="Write Nurbs",
-            description="Write nurbs curves as OBJ nurbs rather than "
-                        "converting to geometry",
-            default=False,
-            )
-    use_vertex_groups = BoolProperty(
-            name="Polygroups",
-            description="",
-            default=False,
-            )
-
-    # grouping group
-    use_blen_objects = BoolProperty(
-            name="Objects as FUS Objects",
-            description="",
-            default=True,
-            )
-    group_by_object = BoolProperty(
-            name="Objects as FUS Groups ",
-            description="",
-            default=False,
-            )
-    group_by_material = BoolProperty(
-            name="Material Groups",
-            description="",
-            default=False,
-            )
-    keep_vertex_order = BoolProperty(
-            name="Keep Vertex Order",
-            description="",
-            default=False,
-            )
-
-    axis_forward = EnumProperty(
-            name="Forward",
-            items=(('X', "X Forward", ""),
-                   ('Y', "Y Forward", ""),
-                   ('Z', "Z Forward", ""),
-                   ('-X', "-X Forward", ""),
-                   ('-Y', "-Y Forward", ""),
-                   ('-Z', "-Z Forward", ""),
-                   ),
-            default='-Z',
-            )
-    axis_up = EnumProperty(
-            name="Up",
-            items=(('X', "X Up", ""),
-                   ('Y', "Y Up", ""),
-                   ('Z', "Z Up", ""),
-                   ('-X', "-X Up", ""),
-                   ('-Y', "-Y Up", ""),
-                   ('-Z', "-Z Up", ""),
-                   ),
-            default='Y',
-            )
-    global_scale = FloatProperty(
-            name="Scale",
-            min=0.01, max=1000.0,
-            default=1.0,
-            )
-
-    path_mode = path_reference_mode
-
-    check_extension = True
-
-    def execute(self, context):
-        from . import export_obj
-
-        from mathutils import Matrix
-        keywords = self.as_keywords(ignore=("axis_forward",
-                                            "axis_up",
-                                            "global_scale",
-                                            "check_existing",
-                                            "filter_glob",
-                                            ))
-
-        global_matrix = (Matrix.Scale(self.global_scale, 4) *
-                         axis_conversion(to_forward=self.axis_forward,
-                                         to_up=self.axis_up,
-                                         ).to_4x4())
-
-        keywords["global_matrix"] = global_matrix
-        return export_obj.save(self, context, **keywords)
-
-
-def menu_func_export(self, context):
-    self.layout.operator(ExportOBJ.bl_idname, text="Fusee (.fus)")
-
+    "version": (0, 0, 1),
+    "blender": (2, 70, 0),
+    "description": "Testing different GUI functions",
+    "category": "User Interface"}
 
 def register():
-    bpy.utils.register_module(__name__)
-    bpy.types.INFO_MT_file_export.append(menu_func_export)
+	print("Register")
+#    bpy.utils.register_module(__name__)
+#    bpy.types.INFO_MT_file_export.append(menu_func_export)
 
 
 def unregister():
-    bpy.utils.unregister_module(__name__)
-    bpy.types.INFO_MT_file_export.remove(menu_func_export)
+	print("Unregister")
+#    bpy.utils.unregister_module(__name__)
+#    bpy.types.INFO_MT_file_export.remove(menu_func_export)
 
-if __name__ == "__main__":
-    register()
+#if __name__ == "__main__":
+#    register()
