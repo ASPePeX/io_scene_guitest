24ee3f727b59c1a5e5f9595dc66dde631f33b5d0
Alexander Scheurer <mail@aspepex.net>
2015-03-31 14:35:19 +0200

    Different inputformats
    Missed commit a few days ago

--- __init__.py
+++ __init__.py
@@ -24,18 +24,77 @@ bl_info = {
     "version": (0, 0, 1),
     "blender": (2, 70, 0),
     "description": "Testing different GUI functions",
-    "category": "User Interface"}
+    "category": "User Interface"
+}
+import bpy
+
+class Bla(bpy.types.Operator):
+    """Bla"""
+    bl_idname = "object.bla"
+    bl_label = "Bla"
+    bl_options = {'REGISTER', 'UNDO'}
+
+# http://www.blender.org/api/blender_python_api_2_73a_release/bpy.props.html
+    booly = bpy.props.BoolProperty(name="My Bool", default=True, subtype='ANGLE')
+    boolyv = bpy.props.BoolVectorProperty(name="My Bool Vector", default=(True,False,True, False), size=4)
+    floaty = bpy.props.FloatProperty(name="My Float", default=0.4527)
+    floatyv= bpy.props.FloatVectorProperty(name="MY Float Vector", size=5)
+    total = bpy.props.IntProperty(name="Steps", default=2, min=1, max=100)  # The only one in use
+    intyv = bpy.props.IntVectorProperty(name="My Int Vector", size=2, default=(2,6))
+    stringy = bpy.props.StringProperty(name="My Sting")
+    stringy = bpy.props.StringProperty(name="My Sting Path", subtype='FILE_PATH')
+
+    def execute(self, context):
+        scene = context.scene
+        cursor = scene.cursor_location
+        obj = scene.objects.active
+
+        for i in range(self.total):
+            obj_new = obj.copy()
+            scene.objects.link(obj_new)
+
+            factor = i / self.total
+            obj_new.location = (obj.location * factor) + (cursor * (1.0 - factor))
+
+        return {'FINISHED'}
+
+def menu_func(self, context):
+    self.layout.operator(Bla.bl_idname)
+
+# store keymaps here to access after registration
+addon_keymaps = []
+
 
 def register():
-	print("Register")
-#    bpy.utils.register_module(__name__)
-#    bpy.types.INFO_MT_file_export.append(menu_func_export)
+    bpy.utils.register_class(Bla)
+    bpy.types.VIEW3D_MT_view.append(menu_func)
+    bpy.types.VIEW3D_MT_object.append(menu_func)
 
+    # handle the keymap
+    wm = bpy.context.window_manager
+    # Note that in background mode (no GUI available), keyconfigs are not available either, so we have to check this
+    # to avoid nasty errors in background case.
+    kc = wm.keyconfigs.addon
+    if kc:
+        km = wm.keyconfigs.addon.keymaps.new(name='Object Mode', space_type='EMPTY')
+        kmi = km.keymap_items.new(Bla.bl_idname, 'SPACE', 'PRESS', ctrl=True, shift=True)
+        kmi.properties.total = 4
+        addon_keymaps.append((km, kmi))
 
 def unregister():
-	print("Unregister")
-#    bpy.utils.unregister_module(__name__)
-#    bpy.types.INFO_MT_file_export.remove(menu_func_export)
+    # Note: when unregistering, it's usually good practice to do it in reverse order you registered.
+    # Can avoid strange issues like keymap still referring to operators already unregistered...
+    # handle the keymap
+    for km, kmi in addon_keymaps:
+        km.keymap_items.remove(kmi)
+    addon_keymaps.clear()
 
-#if __name__ == "__main__":
-#    register()
+	#bpy.utils.unregister_class(BasicMenu)
+    bpy.types.VIEW3D_MT_view.remove(menu_func)
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
+    bpy.utils.unregister_class(Bla)
+
+
+
+if __name__ == "__main__":
+    register()
